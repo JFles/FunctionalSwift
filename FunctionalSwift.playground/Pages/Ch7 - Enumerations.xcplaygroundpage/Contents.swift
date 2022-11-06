3d311c725647deba4c9b1de9743db4c41c7ea1da
Jeremy Fleshman <jeremyfleshman@adt.com>
2022-11-06 08:53:24 -0600

    Continuing work on Ch7

--- FunctionalSwift.playground/Pages/Ch7 - Enumerations.xcplaygroundpage/Contents.swift
+++ FunctionalSwift.playground/Pages/Ch7 - Enumerations.xcplaygroundpage/Contents.swift
@@ -63,9 +63,110 @@ extension Encoding {
 
 //: ## Associated Values
 //:
-//: 
+//: Recalling a past example in Chapter 5, we created a function called `populationOfCapital` which looked up a country's capital, and if it's found, return the capital's population. The result type is `Int?`, and the function returns the population if everything is found, otherwise nil
+//:
+//: One issue here with Swift's optional type is that we don't return an error message when it fails. This leaves us with no way to diagnose what went wrong. Does the country not exist in our dictionary? Is there no population defined for the capital?
+//:
+//: The more meaningful and expressive route would be to return either an `Int` or an `Error`. Luckily, we can accomplish that with Swift Enumerations!
+//:
+//: For our `populationOfCapital` function, instead of returning `Int?`, we can redfine our function to return a case of the `PopulationResult` enum.
+enum LookupError: Error {
+    case capitalNotFound
+    case populationNotFound
+}
 
+enum PopulationResult {
+    case success(Int)
+    case failure(LookupError)
+}
+//: Both of these leverage associated values to return the population as an `Int` for success and a `LookupError` in the case of failure
+//:
+//: Below is our rewrite of `populationOfCapital` which returns a `PopulationResult`. This allows us to explicitly define both our error cases and success for the function.
+let capitals = [
+    "France": "Paris",
+    "Spain": "Madrid",
+    "The Netherlands": "Amsterdam",
+    "Belgium": "Brussels",
+]
+let cities = [
+    "Paris": 2241,
+    "Madrid": 3165,
+    "Amsterdam": 827,
+    "Berlin": 3562
+]
 
+func populationOfCapital(country: String) -> PopulationResult {
+    guard let capital = capitals[country] else {
+        return .failure(.capitalNotFound)
+    }
+    guard let population = cities[capital] else {
+        return .failure(.populationNotFound)
+    }
+    return .success(population)
+}
+//: Callers of this function can use a switch statement to determine whether or not the function succeeded
+switch populationOfCapital(country: "France") {
+case let .success(population):
+    print("France's capital has a population of \(population) thousand inhabitants.")
+case let .failure(error):
+    print("Error: \(error)")
+}
+
+//: ## Adding Generics
+//:
+//: Let's say we want to write a similar function to `populationOfCapital` except we want to look up the mayor of a country's capital
+let mayors = [
+    "Paris": "Hidalgo",
+    "Madrid": "Martinez",
+    "Amsterdam": "Halsema",
+    "Berlin": "Muller"
+]
+//: We could solve this with flatMaps as we did previously in Ch 4
+func mayorOfCapital(country: String) -> String? {
+    return capitals[country]
+        .flatMap { capital in mayors[capital] }
+}
+//: But now we've run into the same issue that our optional return type doesn't tell us why the lookup failed.
+//:
+//: A similar enum to `PopulationResult` would be a good choice here
+enum MayorResult {
+    case success(string)
+    case failure(Error)
+}
+//: But we should see now that the key differences in these enums are the associated values, so we can make a generic `Result` type to cover both enums and future related enums
+enum Result<T, E: Error> {
+    case success(T)
+    case failure(E)
+}
+//: And now we can use our generic `Result` type for both of our functions!
+func populationOfCapital(country: String) -> Result<Int, LookupError> { }
+func mayorOfCapital(country: String) -> Result<String, Error> { }
+
+//: ## Swift Errors
+//:
+//: Swift's built-in error handling is similar to our Result type above. It differs syntactically and semantically, though.
+//:
+//: Syntactically, we define Swift's built-in error with `throws` for the return type, and we must mark calling code with `try`, `try!` or `try?`
+//:
+//: Semantically, the `Result` type is generic over a second error type, so we can be more specific such as with `LookupError`. Swift's built-in error handling is not as precise and only guarantees that an error conforms to the `Error` protocol.
+//:
+//: Below is a rewrite of our `populationOfCapital` function using Swift's errors
+func populationOfCapital(country: String) throws -> Int {
+    guard let capital = capitals[country] else {
+        throw LookupError.capitalNotFound
+    }
+    guard let population = cities[capital] else {
+        throw LookupError.populationNotFound
+    }
+    return population
+}
+//: And to call our function, we can wrap it in a `do-catch` block with a `try` prefix
+do {
+    let population = populationOfCapital(country: "France")
+    print("France's population is \(population)")
+} catch {
+    print("Lookup error: \(error)") // 😟 We don't know the exact Error type here -- unlike `Result<T, E>`
+}
 
 
 
